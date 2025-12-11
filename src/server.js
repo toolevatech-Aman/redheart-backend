6c72e3914b35c4453e49aa9256c9ec066a5beae1
toolevatech-Aman <tooleva.tech@gmail.com>
2025-12-11 22:09:49 +0530

    procfile

--- src/server.js
+++ src/server.js
@@ -1,28 +1,3 @@
-// import dotenv from "dotenv";
-// dotenv.config();
-
-// import express from "express";
-// import {connectDB} from "./config/db.js";
-// import authRoutes from "./routes/authRoutes.js";
-// import productRoutes from "./routes/productRoutes.js";
-
-// const app = express();
-// app.get("/", (req, res) => {
-//   res.send("API Working");
-// });
-
-// app.use(express.json());
-// app.use("/api/auth", authRoutes);
-// app.use("/api/products", productRoutes);
-
-
-// connectDB();
-
-// const PORT = process.env.PORT || 5000;
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
 import dotenv from "dotenv";
 dotenv.config();
 
